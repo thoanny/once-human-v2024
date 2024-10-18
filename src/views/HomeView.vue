6e2311ba8dcb562e0844a10d6ad62073e35fd1f8
Anthony Destenay <anthony.destenay@gmail.com>
2024-10-18 13:47:31 +0200

    Added members

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,9 @@
 import { onMounted, ref, computed } from 'vue';
 
 const specialisations = ref();
+const members = ref();
 const selectedType = ref('');
+const selectedMember = ref('');
 const searchValue = ref('');
 
 const getSpecialisations = async () => {
@@ -11,8 +13,19 @@ const getSpecialisations = async () => {
         .then((data) => data.specialisations);
 };
 
+const getMembers = async () => {
+    return await fetch('https://api.thoanny.fr/once-human/ruche')
+        .then((res) => res.json())
+        .then((data) =>
+            data.members
+                .filter((member) => typeof member.fields['Nom'] !== 'undefined')
+                .sort((a, b) => a.fields['Nom'].localeCompare(b.fields['Nom'])),
+        );
+};
+
 onMounted(async () => {
     specialisations.value = await getSpecialisations();
+    members.value = await getMembers();
 });
 
 const filteredSpecialisations = computed(() => {
@@ -24,6 +37,7 @@ const filteredSpecialisations = computed(() => {
     console.log('s', s);
 
     return specialisations.value
+        .filter((specialisation) => typeof specialisation.fields['Nom'] !== 'undefined')
         .filter((specialisation) =>
             s
                 ? specialisation.fields['Nom']
@@ -41,6 +55,13 @@ const filteredSpecialisations = computed(() => {
         .filter((specialisation) =>
             selectedType.value ? specialisation.fields['Type'] === selectedType.value : true,
         )
+        .filter((specialisation) =>
+            selectedMember.value
+                ? selectedMember.value === 'any'
+                    ? specialisation.fields['Ruche']?.length > 0
+                    : specialisation.fields['Ruche']?.indexOf(selectedMember.value) >= 0
+                : true,
+        )
         .sort((a, b) => a.fields['Nom'].localeCompare(b.fields['Nom']));
 });
 
@@ -55,10 +76,15 @@ const types = computed(() => {
         ),
     ];
 });
+
+const getUsernameById = (id) => {
+    return members.value?.find((member) => member.id === id)?.fields['Nom'] ?? id;
+};
 </script>
 
 <template>
-    <main class="max-w-3xl mx-auto my-6">
+    <!-- <pre>{{ specialisations }}</pre> -->
+    <main class="max-w-4xl mx-auto my-6">
         <h2 class="text-4xl font-semibold text-secondary mb-4">Mimétiques</h2>
         <h3 class="text-3xl font-semibold text-primary mb-4">
             Spécialisations
@@ -81,13 +107,20 @@ const types = computed(() => {
                     v-model="selectedType"
                 >
                     <option value="">---</option>
-                    <option v-for="type in types">{{ type }}</option>
+                    <option v-for="t in types" :key="t.id">{{ t }}</option>
                 </select>
             </div>
             <div class="flex items-center">
                 <div class="badge badge-secondary h-full">Membre</div>
-                <select class="select select-bordered select-sm select-secondary">
+                <select
+                    class="select select-bordered select-sm select-secondary"
+                    v-model="selectedMember"
+                >
                     <option value="">---</option>
+                    <option value="any">N'importe</option>
+                    <option v-for="member in members" :key="member.id" :value="member.id">
+                        {{ member.fields['Nom'] }}
+                    </option>
                 </select>
             </div>
         </div>
@@ -96,6 +129,7 @@ const types = computed(() => {
             <li
                 v-for="specialisation in filteredSpecialisations"
                 :key="specialisation.id"
+                :data-id="specialisation.id"
                 class="flex gap-3 items-center bg-neutral bg-opacity-25 px-4 py-3"
             >
                 <img
@@ -104,8 +138,22 @@ const types = computed(() => {
                     onerror="this.src='/specialisations/default.png'"
                 />
                 <div class="flex flex-col flex-1 gap-1">
-                    <div class="font-bold text-white text-lg">
-                        {{ specialisation.fields['Nom'] }} ({{ specialisation.id }})
+                    <div class="font-bold text-white text-lg flex gap-2 items-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            class="size-6 text-success"
+                            v-if="specialisation.fields['Validé']"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M16.403 12.652a3 3 0 0 0 0-5.304 3 3 0 0 0-3.75-3.751 3 3 0 0 0-5.305 0 3 3 0 0 0-3.751 3.75 3 3 0 0 0 0 5.305 3 3 0 0 0 3.75 3.751 3 3 0 0 0 5.305 0 3 3 0 0 0 3.751-3.75Zm-2.546-4.46a.75.75 0 0 0-1.214-.883l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+
+                        {{ specialisation.fields['Nom'] }}
                     </div>
                     <div>
                         {{ specialisation.fields['Description'] }}
@@ -116,9 +164,9 @@ const types = computed(() => {
                         </span>
                         <span
                             class="badge badge-secondary"
-                            v-for="user in specialisation.fields['Ruche']"
+                            v-for="userId in specialisation.fields['Ruche']"
                         >
-                            {{ user }}
+                            {{ getUsernameById(userId) }}
                         </span>
                     </div>
                 </div>
